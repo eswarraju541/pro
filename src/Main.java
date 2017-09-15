ceac3944f88370e0591bac2aca8ee5780ba09d56
eswarraju541 <31954833+eswarraju541@users.noreply.github.com>
2017-09-15 22:59:54 +0530

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner sn=new Scanner(System.in);
+int length=sn.nextInt();
+StudentGroup sg=new StudentGroup(length);
+
+	}
+
+}
